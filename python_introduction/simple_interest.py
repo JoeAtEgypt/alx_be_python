5ba891a19d16be26469fd766b3334921b2213c51
Youssef Shaker <youssefaymanshaker@outlook.com>
2025-05-25 14:35:16 +0300

    incorrect principal

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-principle = 1000  # Principal amount in dollars
+principal = 1000  # Principal amount in dollars
 rate = 5  # Interest rate in percent
 time = 3  # Time in years
 
 # Calculate simple interest
-simple_interest = (principle * rate * time) / 100
+simple_interest = (principal * rate * time) / 100
 print(f"The simple interest is: {simple_interest}")
