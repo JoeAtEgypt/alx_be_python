30782b53acd8904f13ebc39639edc80893345962
Youssef Shaker <youssefaymanshaker@outlook.com>
2025-05-25 14:31:54 +0300

    added and subtracted and multiplicated and divided the arguments

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+# Addition
+result_addition = number1 + number2
+# Subtraction
+result_subtraction = number1 - number2
+# Multiplication
+result_multiplication = number1 * number2
+# Division  
+result_division = number1 / number2
+
+print(f"Addition of {number1} and {number2} is", result_addition)
+print(f"Subtraction of {number1} and {number2} is", result_subtraction)
+print(f"Multiplication of {number1} and {number2} is", result_multiplication)
+print(f"Division of {number1} and {number2} is", result_division)
